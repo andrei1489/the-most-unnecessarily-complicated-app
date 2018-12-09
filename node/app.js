cf88fc47f8b74fabdc72a04c7af3732a89521120
Andrei Dumitrescu <andrei.r.dumitrescu@gmail.com>
2018-12-09 20:23:47 +0200

    * gave up on nestjs and went with standard node/expressjs

---
+++ node/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.use('/', express.static('www'));
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
+
+var redis = require('redis');
+var url = "course-redis";
+var client1 = redis.createClient(6379, url);
+
+client1.on('message', function(chan, msg) {
+    io.sockets.emit("messages",msg);
+    console.log("emitted message:"+msg);
+});
+
+client1.subscribe('messages');
